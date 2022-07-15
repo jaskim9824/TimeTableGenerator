d9890a816bbc61f7bd8df41f80ecc5568ade02a1
Zachary Schmidt <zacharyaschmidt@hotmail.ca>
2022-07-15 10:36:36 -0600

    Added documentation

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+# Authors: Jason Kim, Zachary Schmidt
+# Collaborators: Moaz Abdelmonem
+# Oversight: Dr. David Nobes
+# University of Alberta, Summer 2022, Curriculum Development Co-op Term
+
+# This file is the main script for the generation of the timetable.
+# The program will parse the provided Excel files containing course
+# and plan information to programatically generate an interactive timetable
+# diagram in the output directory.
+
+# Dependencies: bs4, courseparsing, coursegroupparsing, htmlgen, javascriptgen
+
+from bs4 import BeautifulSoup
 import courseparsing
 import coursegroupparsing
-from bs4 import BeautifulSoup
 import htmlgen
 import javascriptgen
 
@@ -9,8 +21,10 @@ def main():
         with open("template.html") as input:
             controller = open("./output/js/controller.js", "w")
             soup = BeautifulSoup(input, 'html.parser')
+
+            # parsing the Excel files
             courseObjDict, sequenceDict = courseparsing.parseCourses("TimeTable.xls", "Sequencing.xls")
-            deptName = "Test Department"
+            deptName = "Test Department"  # hardcoded for now
 
             # extracting course group information
             courseGroupDict = coursegroupparsing.extractPlanCourseGroupDict(sequenceDict)
@@ -18,6 +32,7 @@ def main():
             intitalCourseGroupVals = coursegroupparsing.findIntitalValuesofCourseGroups(courseGroupDict, courseGroupList)
             optionDict = coursegroupparsing.extractingListofOptions(sequenceDict)
 
+            # writing JS controller
             javascriptgen.intializeControllerJavaScript(sequenceDict, 
                                                         intitalCourseGroupVals,
                                                         courseGroupDict,
@@ -30,7 +45,7 @@ def main():
             #locating main div, this is where all the html will be written
             mainTag = soup.body.find("div", id="main")
 
-            htmlgen.switchTitle(titleTag, topTitleTag, deptName)
+            htmlgen.switchTitle(titleTag, topTitleTag, deptName)  # changing page title based on deptName
 
             formTag = mainTag.find(id = "planlegend")
 
@@ -38,12 +53,15 @@ def main():
 
             courseGroupTag = mainTag.find(id = "optionlegend")
 
+            # radio inputs for plan, term, and course group
             htmlgen.placeRadioInputs(formTag, termTag, courseGroupTag, optionDict, soup)
 
+            # main tag holding timetable itself
             displayTag = htmlgen.generateDisplayDiv(soup, courseGroupList)
 
             mainTag.append(displayTag)
 
+            # generating html for each plan & each term
             htmlgen.placePlanDivs(displayTag, 
                                   sequenceDict, 
                                   soup)
@@ -60,7 +78,7 @@ def main():
     return soup
 
 def writingHTML(soup):
-    # writing output to an output html
+    # writing output string to an output html file
     try:
         print("Writing final HTML.....")
         with open("./output/index.html", "w", encoding="utf-8") as output:
