0d1a99b0e9f89accd25fe4fd3129c72424beea20
Moaz Abdelmonem <mabdelmo@ualberta.ca>
2022-08-09 10:25:26 -0600

    Added Browse Functions & menu

--- main.py
+++ main.py
@@ -22,9 +22,10 @@ from tkinter import ttk
 from tkinter import filedialog
 from PIL import ImageTk, Image
 
+######Main GUI Window #######
 window = Tk()
 window.title('Timetable Generator')
-window.iconbitmap('output/images/favicon.ico')
+window.iconbitmap('GUI_images/favicon.ico')
 window.geometry("1130x595")
 window.configure(bg = "#ffffff")
 canvas = Canvas(
@@ -37,6 +38,15 @@ canvas = Canvas(
     relief = "ridge")
 canvas.place(x = 0, y = 0)
 window.resizable(False, False)
+
+def writingHTML(soup):
+    # writing output string to an output html file
+    try:
+        print("Writing final HTML.....")
+        with open("./output/index.html", "w", encoding="utf-8") as output:
+            output.write(str(soup))
+    except FileNotFoundError:
+        raise FileNotFoundError("The directory you are in does not have a directory named output.")
 
 def main():
     try:
@@ -102,18 +112,180 @@ def main():
        else:
         raise FileNotFoundError(str(err))
 
-    return soup
+    writingHTML(soup)
+    
 
-def writingHTML(soup):
-    # writing output string to an output html file
-    try:
-        print("Writing final HTML.....")
-        with open("./output/index.html", "w", encoding="utf-8") as output:
-            output.write(str(soup))
-    except FileNotFoundError:
-        raise FileNotFoundError("The directory you are in does not have a directory named output.")
+
+def show(selection):
+    department.delete(0, END)
+    department.insert(tkinter.END, selection) 
+
+
+########Timetable excel file UI########
+tableEntry_img = PhotoImage(file = f"GUI_images/img_textBox0.png")
+tableEntry_bg = canvas.create_image(
+    826.5, 151.5,
+    image = tableEntry_img)
+
+tableExcel = Entry(
+    bd = 0,
+    bg = "#d9d9d9",
+    highlightthickness = 0,
+    font='halvetica 12')
+tableExcel.insert(tkinter.END, "")
+tableExcel.place(
+    x = 687, y = 134,
+    width = 279,
+    height = 33)
+
+########Sequencing excel file UI########
+seqEntry_img = PhotoImage(file = f"GUI_images/img_textBox1.png")
+seqEntry_bg = canvas.create_image(
+    826.5, 226.5,
+    image = seqEntry_img)
+
+seqExcel = Entry(
+    bd = 0,
+    bg = "#d9d9d9",
+    highlightthickness = 0,
+    font='halvetica 12')
+seqExcel.insert(tkinter.END, "")
+seqExcel.place(
+    x = 687, y = 209,
+    width = 279,
+    height = 33)
+
+########Accreditation Excel file UI########
+accrEntry_img = PhotoImage(file = f"GUI_images/img_textBox2.png")
+accrEntry_bg = canvas.create_image(
+    826.5, 304.5,
+    image = accrEntry_img)
+
+accrExcel = Entry(
+    bd = 0,
+    bg = "#d9d9d9",
+    highlightthickness = 0,
+    font='halvetica 12')
+accrExcel.insert(tkinter.END, "")
+accrExcel.place(
+    x = 687, y = 287,
+    width = 279,
+    height = 33)
+
+########department name UI########
+deptEntry_img = PhotoImage(file = f"GUI_images/img_textBox3.png")
+deptEntry_bg = canvas.create_image(
+    826.5, 382.5,
+    image = deptEntry_img)
+
+department = Entry(
+    bd = 0,
+    bg = "#d9d9d9",
+    highlightthickness = 0,
+    font='halvetica 12')
+department.insert(tkinter.END, "")
+department.place(
+    x = 687, y = 365,
+    width = 279,
+    height = 33)
+
+########deptNames menu########
+menubutton = tkinter.Menubutton(window, text="Select", font='Helvatica 13',
+                           borderwidth=0, relief="raised",
+                           indicatoron=True, bg='#5D1695',fg='White', border=3)
+deptMenu = tkinter.Menu(menubutton, tearoff=False)
+menubutton.configure(menu=deptMenu)
+deptMenu.add_radiobutton(label="Chemical Engineering", font='halvetica 12', command=lambda: show('Chemical Engineering'))
+deptMenu.add_radiobutton(label="Civil Engineering", font='halvetica 12', command= lambda:show('Civil Engineering'))
+deptMenu.add_radiobutton(label="Computer Engineering", font='halvetica 12', command= lambda:show('Computer Engineering'))
+deptMenu.add_radiobutton(label="Electrical Engineering", font='halvetica 12', command= lambda:show('Electrical Engineering'))
+deptMenu.add_radiobutton(label="Engineering Physics", font='halvetica 12',command= lambda:show('Engineering Physics'))
+deptMenu.add_radiobutton(label="Materials Engineering", font='halvetica 12',command= lambda:show('Materials Engineering'))
+deptMenu.add_radiobutton(label="Mechanical Engineering", font='halvetica 12',command= lambda:show('Mechanical Engineering'))
+deptMenu.add_radiobutton(label="Mechatronics Engineering", font='halvetica 12',command= lambda:show('Mechatronics Engineering'))
+deptMenu.add_radiobutton(label="Mining Engineering", font='halvetica 12',command= lambda:show('Mining Engineering'))
+deptMenu.add_radiobutton(label="Petroleum Engineering", font='halvetica 12',command= lambda:show('Petroleum Engineering'))
+
+menubutton.place(x=1005, y=365)
+
+background_img = PhotoImage(file = f"GUI_images/background.png")
+background = canvas.create_image(
+    467.5, 297.5,
+    image=background_img)
+
+
+def btn_clicked():
+    print("Button Clicked")
+
+######browse functions######
+def tableBrowse():
+    filename =filedialog.askopenfilename()
+    tableExcel.delete(0, END)
+    tableExcel.insert(tkinter.END, filename) 
+
+def seqBrowse():
+    filename =filedialog.askopenfilename()
+    seqExcel.delete(0, END)
+    seqExcel.insert(tkinter.END, filename) 
+
+def accBrowse():
+    filename =filedialog.askopenfilename()
+    accrExcel.delete(0, END)
+    accrExcel.insert(tkinter.END, filename) 
+
+######Browse buttons######
+browseImg1 = PhotoImage(file = f"GUI_images/img1.png")
+button1_excel = Button(
+    image = browseImg1,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = tableBrowse,
+    relief = "flat")
+
+button1_excel.place(
+    x = 1001, y = 134,
+    width = 95,
+    height = 37)
+
+browseImg2 = PhotoImage(file = f"GUI_images/img2.png")
+button2_excel = Button(
+    image = browseImg2,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = seqBrowse,
+    relief = "flat")
+
+button2_excel.place(
+    x = 1001, y = 213,
+    width = 95,
+    height = 38)
+
+browseImg3 = PhotoImage(file = f"GUI_images/img3.png")
+button3_excel = Button(
+    image = browseImg3,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = accBrowse,
+    relief = "flat")
+
+button3_excel.place(
+    x = 1001, y = 288,
+    width = 95,
+    height = 37)
+
+######Generation button######
+genImg = PhotoImage(file = f"GUI_images/img0.png")
+generate_button = Button(
+    image = genImg,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = main,
+    relief = "flat")
+
+generate_button.place(
+    x = 764, y = 427,
+    width = 126,
+    height = 43)
 
 if __name__ == "__main__":
     window.mainloop()
-    soup = main()
-    writingHTML(soup)
